c2f1249044e9b3957d7628064efdcf30ec695b48
Bennett <bascott1002@gmail.com>
2023-03-05 14:54:36 -0800

    experimented with sorting algorithm

--- src/components/sorting-algorithm.tsx
+++ src/components/sorting-algorithm.tsx
@@ -10,14 +10,15 @@ const SortingAlgorithm: React.FC = () => {
   useEffect(() => {
     let n: number = 0;
     let i: number = 0;
-    let j: number = 0;
+    let j: number = i + 1;
     
     if (n < arr.length) {
       const interval = setInterval(() => {
-        n++;
+      n++;
         if (i < arr.length) {
           setInterval(() => {
             i++;
+            let minIndex: number = i;
             if (j < arr.length) {
               setInterval(() => {
                 j++;
@@ -25,16 +26,16 @@ const SortingAlgorithm: React.FC = () => {
             }
           }, oneSecond);
         }
-        return arr.map<JSX.Element>((keys) => (
-          <div>
-            <svg className='bars'>
-              <rect height={keys * 25} width={50} x={0} y={0}/>
-            </svg>
-          </div>
-        ))
       }, oneSecond);
     }
-  });
+    return arr.map<JSX.Element>((keys) => (
+      <div>
+        <svg className='bars'>
+          <rect height={keys * 25} width={50} x={0} y={0}/>
+        </svg>
+      </div>
+    ))
+  }, []);
 }
 
 export default SortingAlgorithm;
@@ -47,11 +48,7 @@ const inter = Inter({ subsets: ['latin'] })
 const SortingAlgorithm: React.FC = () => {
   const [arr, setArr] = useState([3, 1, 2, 5, 4]);
   const sortedArr = [1, 2, 3, 4, 5]
-
-  useEffect(() => {
-    //TODO: set interval for each individual loop so that the returned JSX represents this information
-    const interval = setInterval(() => {
-      for (let n = 0; n < arr.length; n++) {
+    for (let n = 0; n < arr.length; n++) {
         for (let i = 0; i < arr.length; i++) {
           let minIndex: number = i;
           for (let j = i + 1; j < arr.length; j++) {
@@ -70,13 +67,7 @@ const SortingAlgorithm: React.FC = () => {
             <rect height={keys * 25} width={50} x={-50} y={-50}/>
           </svg>
         </div>
-      ))
-    }, 1000);
-
-    if (arr == sortedArr) {
-      clearInterval(interval);
-    }
-  }, [])
+      )
 }
 
 export default SortingAlgorithm;*/
