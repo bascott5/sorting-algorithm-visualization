ce63ace96e69e2d4a822466754ce6e447126ed3c
Bennett <bascott1002@gmail.com>
2023-03-05 17:30:06 -0800

    changed method of sorting array

--- src/components/sorting-algorithm.tsx
+++ src/components/sorting-algorithm.tsx
@@ -4,49 +4,35 @@ import React, { useState, useEffect } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 const SortingAlgorithm: React.FC = () => {
-  const mappedArr = () => {
-    return arr.map<JSX.Element>((keys) => (
-      <div>
-        <svg className='bars'>
-          <rect height={keys * 25} width={50} x={0} y={0}/>
-        </svg>
-      </div>
-    ))
-  }
-  
+  //TODO: Find a way to make each loop run after every second
   const [arr, setArr] = useState<number[]>([3, 1, 2, 5, 4]);
-  const oneSecond: number = 1000;
 
-  useEffect(() => {
-    let n: number = 0;
-    let i: number = 0;
-    let j: number = i + 1;
-    let minIndex: number = i;
-    
-    if (n < arr.length) {
-      setInterval(() => {
-      n++;
-        if (i < arr.length) {
-          setInterval(() => {
-            i++;
-            minIndex = i;
-            if (j < arr.length) {
-              setInterval(() => {
-                j++;
-                if (arr[j] < arr[minIndex]) {
-                  minIndex = j;
-                }
-              }, oneSecond)
-            }
-          }, oneSecond);
+  //Selection sort
+  while (arr != arr.sort()) {
+    useEffect(() => {
+      for (let i = 0; i < arr.length; i++) {
+        setTimeout(() => {
+        let minIndex: number = i;
+        for (let j = i + 1; j < arr.length; j++) {
+          if (arr[j] < arr[minIndex]) {
+            minIndex = j;
+          }
         }
         let temp: number = arr[i];
         arr[i] = arr[minIndex];
         arr[minIndex] = temp;
-      }, oneSecond);
-    }
-    mappedArr();
-  }, []);
+        }, 1000);
+      }
+    }, []);
+  }
+  
+  return arr.map<JSX.Element>((keys) => (
+    <div>
+      <svg className='bars'>
+        <rect height={keys * 25} width={100 - (arr.length * 10)} x={0} y={0}/>
+      </svg>
+    </div>
+  ))
 }
 
 export default SortingAlgorithm;
