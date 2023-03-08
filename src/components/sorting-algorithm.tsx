77da4c25ba7d0d6eb5e2a2186f90bb19bea495e7
Bennett <bascott1002@gmail.com>
2023-03-07 18:54:34 -0800

    add grid layout and refactored code

--- src/components/sorting-algorithm.tsx
+++ src/components/sorting-algorithm.tsx
@@ -3,9 +3,28 @@ import React, { useState, useEffect } from 'react'
 
 const SortingAlgorithm: React.FC = () => {
   const [arr, setArr] = useState<number[]>([3, 1, 2, 5, 4]);
+  let arrCopy: number[] = [...arr];
 
   //Selection sort
-  while (arr != arr.sort()) {
+  useEffect(() => {
+    for (let i = 0; i < arr.length; i++) {
+      setTimeout(() => {
+      let minIndex: number = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      let temp: number = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+      }, 1000);
+    }
+    
+    console.log(arr);
+  }, [arr])
+
+  /*while (arr != arr.sort()) {
     useEffect(() => {
       for (let i = 0; i < arr.length; i++) {
         const loop = setTimeout(() => {
@@ -21,11 +40,13 @@ const SortingAlgorithm: React.FC = () => {
         }, 1000);
       }
     }, []);
-  }
+  }*/
+
+
   
   return arr.map<JSX.Element>((keys) => 
-    <div className='barsContainer'>
-      <svg className='bars' key={keys.toString()}>
+    <div className='bars'>
+      <svg key={keys.toString()}>
         <rect height={keys * 25} width={100 - (arr.length * 10)} x={arr.indexOf(keys) * 53} y={0}/>
       </svg>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/header';
 import SortingAlgorithm from '@/components/sorting-algorithm'
 import { Inter } from 'next/font/google'
 
 const App: React.FC = () => {
   return(
-    <div className='headerContainer'>
-      <h1 className='header'>🆂🅾🆁🆃🅸🅽🅶 🅰🅻🅶🅾🆁🅸🆃🅷🅼 🆅🅸🆂🆄🅰🅻🅸🆉🅴🆁</h1>
+    <div className='container'>
+      <Header />
       <SortingAlgorithm />
     </div>
   )
