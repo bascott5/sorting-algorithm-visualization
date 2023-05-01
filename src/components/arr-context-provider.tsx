22b1450b52b6837c7f0867c34d7ca65e0da971f2
Bennett <bascott1002@gmail.com>
2023-04-30 19:49:39 -0700

    update visualizer.tsx

--- src/components/arr-context-provider.tsx
+++ src/components/arr-context-provider.tsx
@@ -5,7 +5,7 @@ interface ArrayContextProps {
 }
 
 const initArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-export const arrContext = createContext<[number[], React.Dispatch<SetStateAction<number[]>>]>([initArr, () => initArr]);
+export const arrContext: any = createContext<[number[], React.Dispatch<SetStateAction<number[]>>]>([initArr, () => initArr]);
 
 const ArrContextProvider: React.FC<ArrayContextProps> = ({ children }: ArrayContextProps) => {
   const [arr, setArr] = useState(initArr);
--- src/components/visualizer.tsx
+++ src/components/visualizer.tsx
@@ -4,11 +4,14 @@ import { arrContext } from "./arr-context-provider";
 const Visualizer: React.FC = () => {
   const [arr] = useContext<[number[], React.Dispatch<React.SetStateAction<number[]>>]>(arrContext);
 
-  return arr.map<JSX.Element>((keys) => 
+  return (
     <div>
-      <svg className='bars' key={keys.toString()} viewBox={"0 0 500 500"}>
-        <rect height={keys * 6} width={15} x={arr.indexOf(keys) * 20} y={0} />
-      </svg>
+      {
+        arr.map<JSX.Element>(keys => 
+          <svg className='bars' key={keys.toString()} viewBox={"0 0 500 500"}>
+            <rect height={keys * 6} width={15} x={arr.indexOf(keys) * 20} y={0} />
+          </svg>)
+      }
     </div>
   )
 }
